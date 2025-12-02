87984548cb145d9f96fe1d8d9e9d818be844bd7c
narodocnt <narodocnt@gmail.com>
2025-12-02 21:04:35 +0200

    Update auth-google.js

--- auth-google.js
+++ auth-google.js
@@ -1,5 +1,6 @@
 /* auth-google.js — PKCE для GitHub Pages + n8n */
 
+// const GOOGLE_CLIENT_ID = "225496350184-2v39q3dt1p9k22g52q6ko4vqri7h7tqr.apps.googleusercontent.com";
 const GOOGLE_CLIENT_ID = "225496350184-2v39q3dt1p9k22g52q6ko4vqri7h7tqr.apps.googleusercontent.com";
 const REDIRECT_URI = "https://narodocnt.online/oauth2callback.html"; 
 const N8N_WEBHOOK = "https://narodocnt.online:5678/webhook/google-signup";
